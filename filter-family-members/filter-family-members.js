a29fdf6b25260bec687149cfc3fa085738264411
SofianSaleh <sofiane.saleh11@gmail.com>
2019-11-20 09:37:32 +0100

    nothing

--- filter-family-members/filter-family-members.js
+++ filter-family-members/filter-family-members.js
@@ -12,45 +12,102 @@
 // }
 //
 // EXAMPLE:
-//
-// var familyTree = {
-//   'firstName': 'Beth',
-//   'lastName': 'Johnson',
-//   'location': 'San Francisco',
-//   'children': [
-//     {
-//       'firstName': 'Beth Jr.',
-//       'lastName': 'Johnson',
-//       'location': 'Berkeley',
-//       'children': [
-//         {
-//           'firstName': 'Smitty',
-//           'lastName': 'Won',
-//           'location': 'Beijing',
-//           'children': []
-//         }
-//       ]
-//     },
-//     {
-//       'firstName': 'Joshie',
-//       'lastName': 'Wyattson',
-//       'location': 'Berkeley',
-//       'children': []
-//     }
-//   ]
-// };
-//
-// var livesInBerkeley = function (familyMember) {
-//   return familyMember.location === 'Berkeley';
-// }
-//
-// filterFamilyMembers(familyTree, livesInBerkeley)
+
+var familyTree = {
+  firstName: 'Beth',
+  lastName: 'Johnson',
+  location: 'San Francisco',
+  children: [
+    {
+      firstName: 'Beth Jr.',
+      lastName: 'Johnson',
+      location: 'Berkeley',
+     children: [
+        {
+          firstName: 'Smitty',
+          lastName: 'Won',
+          location: 'Beijing',
+          children: []
+        }
+      ]
+    },
+    {
+      firstName: 'Joshie',
+      lastName: 'Wyattson',
+      location: 'Berkeley',
+      children: []
+    }
+  ]
+};
+
+
+var livesInBerkeley = function (familyMember) {
+  return familyMember['location'] === 'Berkeley';
+}
 //
 // returns ['Beth Jr. Johnson', 'Joshie Wyattson'];
+
+
+// var filterFamilyMembers = function (familyTree, truthTest) {
+//   // All your code in this function body
+//   if(!Array.isArray(familyTree)) {
+//   	var arr = [];
+//   	for(var key in familyTree) {
+//   		arr.push(familyTree[key])
+//   }
+//   arr.slice(1,8)
+//   for(var i = 0; i <= 0; i++){
+//   	//console.log(familyTree['children'].length)
+//   	for(var keys in familyTree['children'][0]) {
+//   		if(familyTree['children'][0] !== []){
+  		
+//   		arr.push(familyTree['children'][0][keys])
+  			
+//   		}
+//   	}
+//   	}//console.log(familyTree['children'][0]['children'])
+//   	for(var i = 0; i = 0; i++){
+//   	//console.log(familyTree['children'].length)
+//   	for(var keyss in familyTree['children'][0]['children']) {
+  		
+  		
+//   		arr.push( familyTree['children'][0]['children'][keyss])
+  			
+  		
+//   	}
+//   	}
+//   }
+//   var tr = truthTest
+//   console.log(tr)
+// //   if(truthTest === arr[0]) {
+// //   	var array = [];
+// //   	array.push[0,1]
+// //   }
+  
+// //   //console.log(arr)
+// //  return filterFamilyMembers(arr.splice(1), truthTest)
+// };
 
 
 
 var filterFamilyMembers = function (familyTree, truthTest) {
   // All your code in this function body
-};
+  var arr = []
+  x = 'Berkeley'
+  for(var key in familyTree) {
+  	if(x === familyTree[key]){
+  		arr.push(familyTree['firstName'])
+  		console.log(familyTree['firstName'])
+  	}
+  }
+  for(var keys in familyTree[key]) {
+  	console.log(familyTree[key][keys]['firstName'])
+  	if(x === familyTree[key][keys]){
+  		arr.push(familyTree[key][keys]['firstName'])
+  	}
+  }return arr
+ }
+filterFamilyMembers(familyTree, livesInBerkeley)
 
+
+// i tried to use recursion but i could'nt figure it out
